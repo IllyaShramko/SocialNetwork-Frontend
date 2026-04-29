cc1b0f501c60549658c046dad6d9c648987527af
IllyaShramko <123illya123123r@gmail.com>
2026-04-29 14:52:14 +0300

    Updated types

--- src/shared/api/types.ts
+++ src/shared/api/types.ts
@@ -6,12 +6,14 @@ export type ApiError = {
 	status: number;
 };
 
-export function isApiError(
-	error: unknown,
-): error is ApiError {
-	return typeof error === "object" && error != null && "status" in error && "data" in error;
+export function isApiError(error: unknown): error is ApiError {
+	return (
+		typeof error === "object" &&
+		error != null &&
+		"status" in error &&
+		"data" in error
+	);
 }
-
 
 export type User = {
 	id: number;
@@ -22,18 +24,56 @@ export type User = {
 	birthday: Date | null;
 	signature: string | null;
 	isActive: boolean;
-	avatars: Avatar[]
+	avatars: Avatar[];
 };
+
 export type Avatar = {
 	image: Image;
 	id: number;
 	imageId: number;
 	userId: number;
-}
+};
+
 export type Image = {
 	id: number;
 	userId: number;
 	filename: string;
 	isVisible: boolean;
 	albumId: number | null;
-}
+	imageId: number | null;
+};
+
+export type Post = {
+	id: number;
+	title: string;
+	topic: string;
+	description: string;
+	views: number;
+	author: User;
+	authorId: number;
+	createdAt: Date;
+	links: Link[];
+	images: Image[];
+	tags: TagPost[];
+	// will be determined on the backend
+	isLiked: boolean;
+	isHearted: boolean;
+};
+
+export type Link = {
+	id: number;
+	postId: number;
+	href: string;
+};
+
+export type TagPost = {
+	id: number;
+	tag: Tag;
+	postId: number;
+	tagId: number;
+};
+
+export type Tag = {
+	id: number;
+	name: string;
+};
